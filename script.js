03a637c664147e7a8b623a2ca0d3cee5ec3d5614
SSWren <sydney.wrenwick@gmail.com>
2022-09-09 20:28:52 -0400

    added questions html and styling with CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+//startGame() => {
+
+}
+
+/*function questionScreen () {
+    let proceed = new Audio();
+    proceed.src = 
+    let questionScreen = document.getElementById("questionScrn")
+}*/
+//nextQuestion()
+//finalResults()
+
+//wrongAnswer()
+    //wrongBruh(noise)
+
+//rightAnswer()
+    rightNice(noise)

960ff2223de91350211c56451f466815e5279e13
SSWren <sydney.wrenwick@gmail.com>
2022-09-10 09:49:44 -0400

    questions added

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 //startGame() => {
 
-}
+
+
+const quiz [
+    {
+        question: He became first African American man to hold the World Heavyweight Champion boxing title in 1908.
+        Option1: Joe Frazier
+        Option2: Muhammad Ali
+        Option3: Jack Johnson
+        Option4: Sugar Ray Leonard
+        Answer: 3
+    }
+
+    {
+        question: In 1966, Marie Van Brittan Brown created an early version of what modern home device still used today?
+        Option1: Home Security System
+        Option2: Thermometer
+        Option3: Vacuum
+        Option4: Dishwasher
+        Answer: 1
+    }
+
+    {
+        question: Phyllis Wheatley published her first poem, making her the first African-American to be published in what year?
+        Option1: 1804
+        Option2: 1910
+        Option3: 1891
+        Option4: 1773
+        Answer: 4
+    }
+
+    {
+        question: Southern states employed the following to circumvent the Constitution to deny suffrage (the right to vote) to African-Americans.
+        Option1: Literacy Tests
+        Option2: Poll Taxes
+        Option3: Violence/Intimidation
+        Option4: All of the Above
+        Answer: 4
+    }
+
+    {
+        question: He, an LGBTQ rights activist, acting as an advisor, educated Martin Luther King, Jr on nonviolent civil resistance tactics after a trip to India in 1948.
+        Option1: James Baldwin
+        Option2: Bayard Rustin
+        Option3: Ron Oden
+        Option4: Phill Wilson
+        Answer: 2
+    }
+    {
+        question: Her performance as “Mammy” in Gone With the Wind (1939) won her Best Supporting Actress at the Oscars that year. However, because of Georgia’s Jim Crow Laws, she was prohibited from attending the ceremony.
+        Option1: Hattie McDaniel
+        Option2: Dorothy Dandridge
+        Option3: Ella Fitzgerald
+        Option4: Ruby Dee
+        Answer: 
+    }
+]
+
+
+
 
 /*function questionScreen () {
     let proceed = new Audio();
